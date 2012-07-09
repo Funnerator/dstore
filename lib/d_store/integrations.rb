832acc7115c6bbe8a5e929533aa57572e106d1cc
Woody Peterson <woody.peterson@gmail.com>
2012-07-08 17:02:17 -0700

    Make ActiveRecord integration work w/ autoload
    Previously, editing the project and reloading Rails would throw undef
    constant errors. With the modules defined as per autoload's preferences,
    this is no longer a problem.

--- lib/d_store/integrations.rb
+++ lib/d_store/integrations.rb
@@ -1,3 +1,10 @@
-if defined?(ActiveRecord)
-  require_relative 'integrations/active_record'
+module DStore
+  module Integrations # to make autoload happy
+    autoload :ActiveRecord, 'd_store/integrations/active_record'
+  end
 end
+
+if defined?(ActiveRecord)
+  ActiveRecord::Base.extend(DStore::Integrations::ActiveRecord)
+end
+
--- lib/d_store/integrations/active_record.rb
+++ lib/d_store/integrations/active_record.rb
@@ -1,15 +1,15 @@
 module DStore
-  module ActiveRecordHook
-    def dstore(column, options = {})
-      if storage_column = options.delete(:in)
-        DStore::MethodBuilder.new(self, storage_column).
-          define_document_accessor(column, options)
-      else
-        serialize column, DStore::DocumentSerializer.new(
-          column, options.merge(:namespace => self.name) )
+  module Integrations
+    module ActiveRecord
+      def dstore(column, options = {})
+        if storage_column = options.delete(:in)
+          ::DStore::MethodBuilder.new(self, storage_column).
+            define_document_accessor(column, options)
+        else
+          serialize column, ::DStore::DocumentSerializer.new(
+            column, options.merge(:namespace => self.name) )
+        end
       end
     end
   end
 end
-
-ActiveRecord::Base.extend(DStore::ActiveRecordHook)
